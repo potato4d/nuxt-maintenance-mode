005c8f81c213f30d517d9eed4981fe0bb4e8f1eb
HANATANI Takuma <mail@potato4d.me>
2019-01-27 17:31:20 +0900

    types: Install TypeScript

--- lib/module.d.ts
+++
@@ -1,9 +0,0 @@
-export interface Option {
-  enable?: boolean
-  route: string
-  matcher?: string
-}
-
-declare function nuxtMaintenanceMode(moduleOption: Option): any;
-
-export default nuxtMaintenanceMode
